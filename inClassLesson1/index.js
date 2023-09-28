0380bde142c932c8e31c4a3761720f9f6e98b15e
Aleksandr_Gubich <a.gubich@bk.ru>
2023-09-28 21:44:40 +0300

    Inclass work 1 improvement

--- inClassLesson1/index.js
+++ inClassLesson1/index.js
@@ -1,34 +1,87 @@
-let secondName, firstName, fathersName, personalData;
-let age, yourAge;
-const MIN_AGE = 1;
-const MAX_AGE = 150
+const MINAGE = 0;
+const MAXAGE = 150;
+const PENSIONMALE = 63;
+const PENSIONFEMALE = 58;
+const SEX_MALE = 'М';
+const SEX_FEMALE = 'Ж';
 
-do {
-    secondName = prompt('Введите вашу фамилию:');
-} while (!secondName);
+const secondName = getUserData('Введите Вашу фамилию', keepWithoutChange, isNotEmptyString);
+const firstName = getUserData('Введите Ваше имя', keepWithoutChange, isNotEmptyString);
+const fathersName = getUserData('Введите Ваше отчество', keepWithoutChange, isNotEmptyString);
+const age = getUserData('Введите Ваш возраст', transformToNumber, isValidAge);
+const sex = getUserData('Укажите Ваш пол буквой "М" или "Ж"', transformToSex, isCorrectSymbol);
+const retiredAge = sex === SEX_MALE ? PENSIONMALE : PENSIONFEMALE;
+const retired = age >= retiredAge ? 'Да' : 'Нет';
 
-do {
-    firstName = prompt('Введите ваше имя:');
-} while (!firstName);
-
-do {
-    fathersName = prompt('Введите ваше отчество:');
-} while (!fathersName);
-
-personalData = `Вас зовут ${secondName} ${firstName} ${fathersName}`;
-
-alert(personalData);
-
-do {
-    let userInput;
+function getUserData(message, transformData, isValid) {
+    let userData = null;
+    let isCanceled = false;
     
     do {
-        userInput = prompt('Введите ваш возраст')
-    } while (!userInput)
+        const rawUserData = prompt(message);
+        
+        isCanceled = rawUserData === null;
+        userData = isCanceled ? null : transformData(rawUserData);
+    } while (isCanceled || !isValid(userData));
 
-    age = Number(userInput)
-} while (!isFinite(age) || age < MIN_AGE || age > MAX_AGE);
+    return userData;
+}
 
-yourAge = `Ваш возраст равен ${age}`;
+function keepWithoutChange(data) {
+    return data;
+}
 
-alert(yourAge);
+function isNotEmptyString(data) {
+    return data;
+}
+
+
+function isValidAge(inputAge) {
+    return Number.isFinite(inputAge) && inputAge >= MINAGE && inputAge <= MAXAGE;
+}
+
+function replaceSymbols(inputString, targetSymbol, replacementSymbol) {
+    let resultString = '';
+
+    for (const char of inputString){
+        resultString += char === targetSymbol ? replacementSymbol : char;
+    }
+
+    return resultString;
+}
+
+function transformToNumber(data) {
+    if (!data) {
+        return NaN;
+    }
+
+    return Number(replaceSymbols(data, ',', '.'));
+}
+
+function transformToSex(sexInput) {
+    switch (sexInput) {
+        case 'м':
+        case 'М':
+        case 'm':
+        case 'M':
+            return SEX_MALE;
+        case 'ж':
+        case 'Ж':
+        case 'f':
+        case 'F':    
+            return SEX_FEMALE;
+        default:
+            return '';
+    }
+}
+
+function isCorrectSymbol(someSex) {
+    return someSex === SEX_MALE || someSex === SEX_FEMALE;
+}
+
+alert (
+`ФИО: ${secondName} ${firstName} ${fathersName}
+Возраст: ${age}
+Пол: ${sex}
+На пенсии: ${retired}`
+);
