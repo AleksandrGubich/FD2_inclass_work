a8269dd8f17cf3a536600edd7dead69af925aa95
Aleksandr_Gubich <a.gubich@bk.ru>
2023-10-03 21:42:49 +0300

    Inclasswork 2

---
+++ inClassLesson2/index.js
@@ -0,0 +1,15 @@
+const stringToCheck = 'giwhedfedsgdfsjsbwrfgwcer'
+
+function symbolsCount(string) {
+    
+    let objectsAmount = {};
+
+    for (const char of string) {
+        const currentCounter = objectsAmount[char] ?? 0;
+        objectsAmount[char] = currentCounter + 1;
+    }
+
+    return objectsAmount;
+}
+
+console.log(symbolsCount(stringToCheck))
