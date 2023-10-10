5ac77d089adae30be62ebab416f49a6153aadd41
Aleksandr_Gubich <a.gubich@bk.ru>
2023-10-10 21:49:23 +0300

    Inclasswork 4

---
+++ inClassLesson4/index.js
@@ -0,0 +1,74 @@
+// // ЗАДАЧА 1
+// function task1() {
+//     for (i = 1; i <= 50; i++){
+//         console.log(i)
+//     }
+// }
+
+// console.log(task1())
+
+
+
+// // ЗАДАЧА 2
+// function arrayLog(arr) {
+//     for (i = 0; i < arr.length; i++) {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+
+// // ЗАДАЧА 3
+// function arrMultiplier(arr) {
+//     let counter = 1;
+
+//     for (i = 0; i <arr.length; i++) {
+//         counter *= arr[i];
+//     }
+
+//     return counter;
+// }
+// // ИЛИ
+// function arrMultiplier(arr) {
+//     return arr.reduce((el, index) => 
+//         el*index)
+// }
+
+
+
+// // ЗАДАЧА 4
+// function correctNumbers(arr) {
+//     return arr.filter((num) => 
+//         num >= 0 && num <= 100 && (num % 2 === 0)
+//     )
+// }
+
+
+
+// // ЗАДАЧА 5
+// let someNumber = 1000;
+// let counter = 0;
+
+// while (someNumber >= 50) {
+//     someNumber /= 2;
+//     counter++;
+// }
+
+// console.log(counter)
+
+// ЗАДАЧА 6
+function yearNumSumm() {
+    const yearArray = {};
+
+    for (i = 49; i < arr.length; i++) {
+        let numToSplit = [i]
+        let splittedNum1 = 0
+        let splittedNum2 = 0
+        let splittedNum3 = 0
+
+        if (String.numToSplit.length === 2){
+            
+        }
+    }
+
+}
