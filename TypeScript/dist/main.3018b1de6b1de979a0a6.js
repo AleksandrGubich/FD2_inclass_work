1aea9a9259c94009b1002f63e1a27a1ead908418
Aleksandr Gubich <a.gubich@bk.ru>
2023-11-09 22:20:36 +0300

    Adding TS files

---
+++ TypeScript/dist/main.3018b1de6b1de979a0a6.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************!*\
+  !*** ./index.ts ***!
+  \******************/
+
+alert('ok');
+
+/******/ })()
+;
+//# sourceMappingURL=main.3018b1de6b1de979a0a6.js.map
