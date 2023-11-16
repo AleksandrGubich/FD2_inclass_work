4f52b1914dbe59725e117bfabf8f3a2bd8202401
Aleksandr Gubich <a.gubich@bk.ru>
2023-11-16 21:50:53 +0300

    Typescript first inclass work

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,3 +1,22 @@
-import { greet } from "./greet";
+const mainEl = document.getElementById('main');
+const spanCollection = mainEl ? Array.from(mainEl.querySelectorAll<HTMLElement>('[data-color]')) : [];
 
-greet('Hello world');
+spanCollection.forEach((spanEl) => {
+    const colorValue = spanEl.textContent;
+
+    if (colorValue) {
+        const paintBg = () => {
+            spanEl.style.setProperty('background-color', colorValue);
+
+            setTimeout(() => {
+                spanEl.style.removeProperty('background-color')
+            }, 1_000);
+        };
+
+        spanEl.addEventListener('click', paintBg);
+
+        // setTimeout(() => {
+        //     spanEl.removeEventListener('click', paintBg);
+        // }, 5_000);
+    };
+});
